1795eaf5629a0be2f782f785b25e7fce58642c3e
ihgorek <aemen95@gmail.com>
2017-09-07 10:24:33 +0300

    added register test

--- app/test/register_test.py
+++ app/test/register_test.py
@@ -1,47 +1,40 @@
-# coding=utf-8
 import unittest
-from app.register_and_login import LogIn
-from pymongo import MongoClient
+from app.register_and_login import Register
 
 
 class TestLogIn(unittest.TestCase):
     def setUp(self):
-        self.a = LogIn()
+        self.a = Register()
+        # self.a.del_all()
 
     def tearDown(self):
         del self.a
 
-    def test_login_user(self):
-        self.assertEqual(self.a.login_user('2'), 'bad')
-        self.assertEqual(self.a.login_user('admin'), 'ok')
-        self.assertEqual(self.a.login_user('eva'), 'bad')
-        self.assertEqual(self.a.login_user(''), 'bad')
+    def test_add_user(self):
+        # True
+        self.assertIs(self.a.add_user('eva', 'eva@eva.ru', 'eva123', '/'),
+                      'ok')
+        self.assertIs(self.a.add_user('admin', 'admin@admin.ru', 'admin', '/'),
+                      'ok')
+        # False
+        self.assertEqual(self.a.add_user('admin', 'admin@admin.ru', 'admin', '/'),
+                         'That username is already taken, please choose another')
+        self.assertEqual(self.a.add_user('ad', 'aerd@ared.ru', 'admin123', '/'),
+                         'Username is too short')
+        self.assertEqual(self.a.add_user('adimin', '', 'admin223', '/'),
+                         'Email address is too short, or incorrect')
+        self.assertEqual(self.a.add_user('', '', '', ''),
+                         'Username is too short')
+        self.assertEqual(self.a.add_user('dearfriend', 'eva@eva.ru', '123456', '/'),
+                         'This email is already in use.')
 
-    def test_get_user_id(self):
-        self.assertIs(type(self.a.get_user_id('2')), str)
-        self.assertIs(type(self.a.get_user_id('admin')), int)
-        self.assertIs(type(self.a.get_user_id('')), str)
-
-    def test_get_pwd(self):
-        self.assertIs(self.a.get_pwd(''), 'bad')
-        self.assertIs(self.a.get_pwd('eva'), 'bad')
-        self.assertIs(type(self.a.get_pwd('admin')), unicode)
-
-    def test_change_password(self):
-        pass  # нужен md5 для этого
-
-    def test_del_user(self):
-        self.assertIs(self.a.del_user('eva', 517), None)
-        self.assertIs(self.a.del_user('', 517), None)
-
-    def test_change_email(self):
-
-        self.assertIs(type(self.a.change_email('eva', 517, 'gore@ml')), str)
-        self.assertIs(type(self.a.change_email('admin', 517, 'gara@mailru')), str)
-
-        self.assertIs(self.a.change_email('admin', 1, 'guru@mail.ru'), None)
-        self.assertIs(self.a.change_email('', 1, 'grub@mail.ru'), None)
-        self.assertIs(self.a.change_email('admin', 1, 'admin@admin.ru'), None)
+    def test_get_id_user(self):
+        # True
+        self.assertIsInstance(self.a.get_id_user('admin'),int)
+        self.assertIsInstance(self.a.get_id_user('eva'), int)
+        #False
+        self.assertIsInstance(self.a.get_id_user(''), str)
+        self.assertIsInstance(self.a.get_id_user('random_name'), str)
 
 
 if __name__ == "__main__":
